e8034d96d959c2eacd162decbe41ef22daabdc58
Ravi Shekhar Singh <shekharsingh.ravi@gmail.com>
2017-05-16 09:06:43 +0530

    Completed the problem 2 and updating the solution

--- day1/prob2.py
+++ day1/prob2.py
@@ -1,3 +1,26 @@
 # read the passwd file
 # extract the ordered list of username
 # print alphabetical ordered name & the line number
+
+name = list()
+delimiter = ':'
+
+
+def readfile():
+    fp = open(r'/mnt/blackhat/python/3DayPythonTraining/passwd.txt')
+    for line in fp:
+        user = line.split(delimiter)
+        name.append(user[0])
+    fp.close()
+
+
+def sort_and_print():
+    name.sort()
+    print enumerate(name)
+    for i, c in enumerate(name):
+        print "[{}] --> {}".format(i, c)
+
+
+readfile()
+sort_and_print()
+
